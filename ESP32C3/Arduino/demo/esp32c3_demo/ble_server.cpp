b0f2374bf1dc0450bb166d7a7745c02630cd251a
LiuChao <chao.liu2@medtronic.com>
2025-10-16 10:52:01 +0800

    edit the bletooth channel

--- ESP32C3/Arduino/demo/esp32c3_demo/ble_server.cpp
+++ ESP32C3/Arduino/demo/esp32c3_demo/ble_server.cpp
@@ -8,8 +8,8 @@
  * @copyright Copyright (c) 2025
  * 
  */
-
 #include "ble_server.h"
+#include <Arduino.h>
 #include <string>
 #include <BLEDevice.h>
 #include <BLEUtils.h>
@@ -18,62 +18,97 @@
 #define SERVER_UUID             "12345678-1234-5678-1234-56789abcdef0"
 #define CHARACTERISTIC_UUID     "12345678-1234-5678-1234-56789abcdef1"
 
-// BLEServer* pServer = NULL;
-// BLEService* pService = NULL;
-// BLECharacteristic* pCharacteristic = NULL;
+BLEServer* pServer = nullptr;
+BLEService* pService = nullptr;
+BLECharacteristic* pCharacteristic = nullptr;
 
+bool deviceConnected = false;
 
-class MyCallbacks: public BLECharacteristicCallbacks {
-    void onWrite(BLECharacteristic* pCharacteristic) {
-        String value = pCharacteristic->getValue();
-        // Do something with the value
-        String response = "Received: " + value;
-        if(value.length() > 0)
-        {
-            // pCharacteristic->setValue(response);
-            Serial.println(response.c_str());
+// ---------------- 回调类 ----------------
+class MyServerCallbacks : public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) override {
+        deviceConnected = true;
+        Serial.println("[BLE] Client connected");
+    }
+
+    void onDisconnect(BLEServer* pServer) override {
+        deviceConnected = false;
+        Serial.println("[BLE] Client disconnected, restarting advertising...");
+        BLEDevice::startAdvertising();  // 重新广播
+    }
+};
+
+// 写入特征回调
+class MyCharacteristicCallbacks : public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic* pCharacteristic) override {
+        String rxValue = pCharacteristic->getValue();
+        if (!rxValue.isEmpty()) {
+            // String value = String(rxValue.c_str());
+            String response = "Received: " + rxValue;
+            Serial.println(response);
+
+            // 回复客户端
+            String resp = "Echo: " + rxValue;
+            pCharacteristic->setValue(resp);
+            pCharacteristic->notify();
         }
     }
 };
 
-void ble_server_init(void)
-{
-    // Create the BLE Device
+// ---------------- 初始化 BLE ----------------
+void ble_server_init(void) {
     BLEDevice::init("ESP32C3-ICP");
-    BLEServer* pServer = BLEDevice::createServer();
-    BLEService* pService = pServer->createService(SERVER_UUID);
-    BLECharacteristic* pCharacteristic = pService->createCharacteristic(
+
+    pServer = BLEDevice::createServer();
+    pServer->setCallbacks(new MyServerCallbacks());
+
+    pService = pServer->createService(SERVER_UUID);
+
+    pCharacteristic = pService->createCharacteristic(
         CHARACTERISTIC_UUID,
         BLECharacteristic::PROPERTY_READ |
         BLECharacteristic::PROPERTY_WRITE |
         BLECharacteristic::PROPERTY_NOTIFY
     );
 
-    pCharacteristic->setCallbacks(new MyCallbacks());
-
-    // Start the service
+    pCharacteristic->setCallbacks(new MyCharacteristicCallbacks());
+    pCharacteristic->setValue("Hello World");
     pService->start();
 
-    // Set the characteristic value
-    std::string initialValue = "Hello World";
-    pCharacteristic->setValue(initialValue.c_str());
+    BLEAdvertising* pAdvertising = BLEDevice::getAdvertising();
+    pAdvertising->addServiceUUID(SERVER_UUID);
+    pAdvertising->setScanResponse(true);
+    pAdvertising->setMinPreferred(0x06);
+    pAdvertising->setMaxPreferred(0x12);
+    pAdvertising->start();
 
-    // Advertise the service
-    pServer->getAdvertising()->start();
-    Serial.println("BLE Server started");
-
+    Serial.println("[BLE] Server started and advertising...");
 }
 
-void ble_server_task(void* arg)
-{
+// ---------------- 任务函数 ----------------
+void ble_server_task(void* arg) {
     ble_server_init();
-    
+
+    // 循环保持任务运行
+    while (true) {
+        if (deviceConnected) {
+            pCharacteristic->setValue("Hello from ESP32C3!");
+            pCharacteristic->notify();
+            Serial.println("[BLE] Notified client");
+        }
+        vTaskDelay(pdMS_TO_TICKS(2000));
+    }
 }
 
-
-void ble_server_task_run(void* arg)
-{
-    xTaskCreatePinnedToCore(ble_server_task, "ble_server_task", 4096, NULL, 1, NULL, 0);
-    vTaskDelay(pdMS_TO_TICKS(1000));
-    vTaskDelete(NULL);
+// ---------------- 启动入口 ----------------
+void ble_server_task_run(void* arg) {
+    xTaskCreatePinnedToCore(
+        ble_server_task,
+        "ble_server_task",
+        8192,
+        NULL,
+        1,
+        NULL,
+        0
+    );
 }
--- ESP32C3/Arduino/demo/esp32c3_demo/esp32c3_demo.ino
+++ ESP32C3/Arduino/demo/esp32c3_demo/esp32c3_demo.ino
@@ -20,7 +20,6 @@ void setup() {
   Serial.println("Hello ESP32C3");
   delay(50);
   oled_task_run(nullptr);
-  ble_server_task(nullptr);
 
   run_tasks();
 
@@ -37,5 +36,5 @@ void run_tasks()
   xTaskCreatePinnedToCore(led_task_run, "led_task", 1024, NULL, 1, &led_task_handle, 0);
   // xTaskCreatePinnedToCore(oled_task_run, "oled_task", 4096, NULL, 1, &oled_task_handle, 0);
 
-  // ble_server_task_run(nullptr);
+  ble_server_task_run(nullptr);
 }
