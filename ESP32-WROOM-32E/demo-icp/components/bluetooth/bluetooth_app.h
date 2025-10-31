d28d6cf2e7c1192003304c1e663d791caf28c9c1
LiuChao <chao.liu2@medtronic.com>
2025-10-31 14:53:39 +0800

    add the bluetooth code

---
+++ ESP32-WROOM-32E/demo-icp/components/bluetooth/bluetooth_app.h
@@ -0,0 +1,27 @@
+/**
+ * @file bluetooth_app.h
+ * @author chao.liu (chao.liu2@medtronic.com)
+ * @brief 
+ * @version 0.1
+ * @date 2025-10-31
+ * 
+ * @copyright Copyright (c) 2025
+ * 
+ */
+#ifndef __BLUETOOTH_APP_H_ 
+#define __BLUETOOTH_APP_H_
+
+#include <stdint.h>
+#include <string.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <inttypes.h>
+
+
+
+
+void bluetooth_app_start(void);
+
+
+
+#endif  // __BLUETOOTH_APP_H_
--- ESP32-WROOM-32E/demo-icp/main/demo-icp.c
+++ ESP32-WROOM-32E/demo-icp/main/demo-icp.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
+#include "nvs.h"
+#include "nvs_flash.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_system.h"
 #include "sensor_comm.h"
+#include "bluetooth_app.h"
 
 
 void app_main(void)
 {
-    sensor_comm_init();
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_ERROR_CHECK(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    ESP_ERROR_CHECK( ret );
+
+
+    bluetooth_app_start();
+    // sensor_comm_init();
 
     while (1)
     {
